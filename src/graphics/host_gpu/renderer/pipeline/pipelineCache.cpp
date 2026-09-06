15bca2d759d87ce941ee9c07e4fb88dc9ab209f8
TarkusR <90155059+TarkusR@users.noreply.github.com>
2026-09-07 00:12:29 +0200

    renderer: name shader modules by guest hash

--- src/graphics/host_gpu/renderer/pipeline/pipelineCache.cpp
+++ src/graphics/host_gpu/renderer/pipeline/pipelineCache.cpp
@@ -275,6 +275,8 @@ struct PipelineCache::ProgramCache {
 		RequireVulkanSuccess(device.createShaderModule(&create_info, nullptr, &module),
 		                     "create recompiled shader module");
 		EXIT_IF(module == nullptr);
+		SetVulkanObjectNameF(device, module, "Kyty.Shader.{}[0x{:016x}]", stage_name,
+		                     options.shader_hash);
 		if (options.dump_ir) {
 			if (!options.early_dump) {
 				LOGF("%s decoded RDNA2:\n%s", options.dump_label, result.decoded_dump.c_str());
